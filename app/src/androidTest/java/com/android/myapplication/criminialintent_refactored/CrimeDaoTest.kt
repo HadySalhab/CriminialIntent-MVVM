e860714d08d92bb1b9aad36805c07fc5f6639d9d
Hady Salhab <hadysalhab1@gmail.com>
2020-01-31 01:17:18 +1100

    Testing Queries of the persistence layer

---
+++ app/src/androidTest/java/com/android/myapplication/criminialintent_refactored/CrimeDaoTest.kt
@@ -0,0 +1,95 @@
+package com.android.myapplication.criminialintent_refactored
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.android.myapplication.criminialintent_refactored.database.CrimeDao
+import com.android.myapplication.criminialintent_refactored.database.CrimeDb
+import com.android.myapplication.criminialintent_refactored.database.CrimeEntity
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.*
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(AndroidJUnit4::class)
+class CrimeDaoTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var crimeDb: CrimeDb
+    private lateinit var SUT: CrimeDao
+
+    @Before
+    fun setUp() {
+        crimeDb = Room.inMemoryDatabaseBuilder(
+            InstrumentationRegistry.getInstrumentation().context,
+            CrimeDb::class.java
+        ).build()
+        SUT = crimeDb.crimeDao()
+    }
+
+    @After
+    fun closeDb() {
+        crimeDb.close()
+    }
+
+    @Test
+    fun allCrimes_emptyDB_shouldReturnEmptyList(){
+        val testObserver:Observer<List<CrimeEntity>> = mock()
+
+        SUT.allCrimes().observeForever(testObserver)
+
+        verify(testObserver).onChanged(emptyList())
+    }
+
+    @Test
+    fun addCrime_shouldSaveCrimeToDb(){
+        val crime = CrimeEntity()
+        val testObserver:Observer<List<CrimeEntity>> = mock()
+
+        SUT.addCrime(crime)
+
+        SUT.allCrimes().observeForever(testObserver)
+        val listClass = ArrayList::class.java as Class<ArrayList<CrimeEntity>>
+        val argumentCaptor = ArgumentCaptor.forClass(listClass)
+        verify(testObserver).onChanged(argumentCaptor.capture())
+        Assert.assertTrue(argumentCaptor.allValues.size>0)
+
+    }
+
+    @Test
+    fun allCrimes_dbNotEmpty_shouldReturnCorrectData(){
+        val crime1 = CrimeEntity()
+        val crime2 = CrimeEntity()
+        SUT.addCrime(crime1)
+        SUT.addCrime(crime2)
+        val testObserver:Observer<List<CrimeEntity>> = mock()
+
+        SUT.allCrimes().observeForever(testObserver)
+
+        val listClass = ArrayList::class.java as Class<ArrayList<CrimeEntity>>
+        val argumentCaptor = ArgumentCaptor.forClass(listClass)
+        verify(testObserver).onChanged(argumentCaptor.capture())
+        val capturedArgument = argumentCaptor.value
+        Assert.assertTrue(capturedArgument.containsAll(listOf(crime1,crime2)))
+
+    }
+
+    @Test
+    fun getCrime_shouldReturnCorrectData(){
+        val crime1 = CrimeEntity()
+        SUT.addCrime(crime1)
+        val testObserver:Observer<CrimeEntity?> = mock()
+
+        SUT.getCrime(crime1.id).observeForever(testObserver)
+
+        verify(testObserver).onChanged(eq(crime1))
+    }
+
+
+}
