5e4a2be6ced7a53c201cd5ed02b11cae93e9f05f
Hady Salhab <hadysalhab1@gmail.com>
2020-01-30 17:39:42 +1100

    Adding the Model

---
+++ app/src/main/java/com/android/myapplication/criminialintent_refactored/Crime.kt
@@ -0,0 +1,14 @@
+package com.android.myapplication.criminialintent_refactored
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity
+data class Crime(@PrimaryKey val id: String = UUID.randomUUID().toString(),
+                 var title: String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false,
+                 var suspect: String = "") {
+
+}
