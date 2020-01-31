8abd56b46769cf63ce7aed074688e4d81f9ea255
Hady Salhab <hadysalhab1@gmail.com>
2020-01-31 16:39:18 +1100

    Setting Koin

---
+++ app/src/main/java/com/android/myapplication/criminialintent_refactored/App.kt
@@ -0,0 +1,35 @@
+package com.android.myapplication.criminialintent_refactored
+
+import android.app.Application
+import com.android.myapplication.criminialintent_refactored.database.CrimeDb
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+
+class App : Application() {
+    private val koinModule = module {
+
+        single {
+            //Database
+            CrimeDb.newInstance(androidContext())
+        }
+        single {
+            val db: CrimeDb = get()
+            Repository(db.crimeDao(), DataTransformer())
+        }
+        viewModel {
+            ListCrimesViewModel(get())
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@App)
+            modules(koinModule)
+        }
+    }
+}
