ec24befb18e98843341c34c055345570ac9c2e76
Hady Salhab <hadysalhab1@gmail.com>
2020-02-01 23:23:22 +1100

    adding CrimeDetailViewModelTest

--- app/src/test/java/com/android/myapplication/criminialintent_refactored/CrimeDetailViewModelTest.kt
+++ app/src/test/java/com/android/myapplication/criminialintent_refactored/CrimeDetailViewModelTest.kt
@@ -2,24 +2,39 @@ package com.android.myapplication.criminialintent_refactored
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.eq
 import org.mockito.Mockito.mock
 import java.util.*
 
 class CrimeDetailViewModelTest{
 
-/*    @get:Rule
+    @get:Rule
     var instantTaskExecutorRule = InstantTaskExecutorRule()
     private val CRIME_ID = UUID.randomUUID().toString()
     private lateinit var repoMock:Repository
     private lateinit var SUT:CrimeDetailViewModel
 
-    @Before
-    fun setUp() {
+
+    @Test
+    fun initializeCrime_idNotNull_shouldCallRepo()= runBlocking {
         repoMock = mock()
         SUT = CrimeDetailViewModel(repoMock,CRIME_ID)
+
+
+
+        val job = launch {
+            SUT.initializeCrime()
+            verify(repoMock).loadCrime(ArgumentMatchers.eq(CRIME_ID))
+        }
+        job.cancel()
+
     }
-    */
 }
